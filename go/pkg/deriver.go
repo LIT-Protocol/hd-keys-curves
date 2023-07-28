a476cf6881a0412614fd1e7358244e9f1647d753
Michael Lodder <mike@litprotocol.com>
2023-07-28 11:07:56 -0600

    return raw byte array

--- go/pkg/deriver.go
+++ go/pkg/deriver.go
@@ -57,9 +57,7 @@ func (d *DerivePublicKey) Run(input []byte) ([]byte, error) {
 	}
 
 	derivedKey := deriver.ComputePublicKey(params.rootKeys)
-	output := derivedKey.ToAffineCompressed()
-	output = append([]byte{byte(len(output))}, output...)
-	return output, nil
+	return derivedKey.ToAffineCompressed(), nil
 }
 
 type deriveParams struct {
