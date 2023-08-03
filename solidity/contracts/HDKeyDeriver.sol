c4e36ac2e56efe07c27320b95c9e17c3fbe8878c
Josh Long <josh@litprotocol.com>
2023-08-03 13:53:08 -0400

    cleanup

--- solidity/contracts/HDKeyDeriver.sol
+++ solidity/contracts/HDKeyDeriver.sol
@@ -25,35 +25,38 @@ abstract contract KeyDeriver {
         bytes32 derivedKeyId,
         RootKey[] memory rootHDKeys
     ) public view returns (bool, bytes memory) {
-        bytes memory args = buildArgs(derivedKeyId, rootHDKeys);
+        bytes memory args = _buildArgs(derivedKeyId, rootHDKeys);
         (bool success, bytes memory data) = HD_KDF.staticcall(args);
         return (success, data);
     }
 
-    function buildArgs(
+    function _buildArgs(
         bytes32 derivedKeyId,
         RootKey[] memory rootHDKeys
-    ) public pure returns (bytes memory) {
+    ) private pure returns (bytes memory) {
         // empty array for concating pubkeys
         bytes memory rootPubkeys = new bytes(0); // each key is 33 bytes
         for (uint256 i = 0; i < rootHDKeys.length; i++) {
-            // console.log(rootHDKeys[i].pubkey.length);
             rootPubkeys = rootPubkeys.concat(rootHDKeys[i].pubkey);
         }
 
-        uint8 kt = uint8(rootHDKeys[0].keyType);
+        uint8 keyType = uint8(rootHDKeys[0].keyType);
         bytes memory CTX = bytes(HD_KDF_CTX);
-        bytes memory args_bytes = abi.encodePacked(
-            bytes1(kt), // 1st arg is a byte for the curve type, 0 is Nist Prime256, 1 is secp256k1
-            bytes4(uint32(32)), // 2nd arg is a 4 byte big-endian integer for the number of bytes in id
+	    bytes1 kt = bytes1(keyType);
+        bytes4 id_len = bytes4(uint32(derivedKeyId.length));
+        bytes4 ctx_len = bytes4(uint32(CTX.length));
+        bytes4 pubkey_len = bytes4(uint32(rootHDKeys.length));
+                
+	    bytes memory args_bytes = abi.encodePacked( 
+            kt, // 1st arg is a byte for the curve type, 0 is Nist Prime256, 1 is secp256k1
+            id_len, // 2nd arg is a 4 byte big-endian integer for the number of bytes in id
             derivedKeyId, // 3rd arg is the byte sequence for id
-            bytes4(uint32(CTX.length)), // 4th arg is a 4 byte big-endian integer for the number of bytes in cxt
+            ctx_len, // 4th arg is a 4 byte big-endian integer for the number of bytes in cxt
             CTX, // 5th arg is the byte sequence for cxt
-            bytes4(uint32(rootHDKeys.length)), // 6th arg is a 4 byte big-endian integer for the number of root keys
+            pubkey_len, // 6th arg is a 4 byte big-endian integer for the number of root keys
             rootPubkeys // 7th arg is a variable number of root keys each 33 bytes in length
         );
-       // console.log("========built arguments========");
-       // console.logBytes(args_bytes);
+
         return args_bytes;
     }
 }
--- solidity/test/TestHDKeyDeriver.sol
+++ solidity/test/TestHDKeyDeriver.sol
@@ -4,19 +4,22 @@ import "../contracts/HDKeyDeriver.sol";
 contract HDKeyDeriverTest is KeyDeriver {
 
     function testHDKeyDerive() public view returns (bytes memory){
-        RootKey[] memory pubkeys = new RootKey[](3);
-        for (uint256 i = 0; i < 3; i++) {
-            pubkeys[i].pubkey = hex"04127644144de44ab1962e32641e64cf5472f01bd0d6d04e6feb4e213a96e58ef3b0db72d036ba5bf8b59fc47583623bb6eb2943b040ee27f3b52a76f123ff022e";
-            pubkeys[i].keyType = 2;
-        }
+        RootKey[] memory pubkeys = new RootKey[](2);
+       
+        pubkeys[0].pubkey = hex"02706ed9fbf152fcc24fa744f727fb3f1e309344f458f6f1ce5ac395785c40b758";
+        pubkeys[0].keyType = 1;
+        pubkeys[1].pubkey = hex"0248a534627a648dc2f3a555ae215d887a38d1983b962a32215a4c8ab01817aed0";
+	    pubkeys[1].keyType = 1;
+        
         bytes32 keyId = keccak256("discord_handle:1234");
         (bool result, bytes memory pubkey) = computeHDPubKey(keyId, pubkeys);
 
         if (!result) {
-            // error out?
+            revert("result was false, error while generating pubkey");
         }
 
         return pubkey;
     }
 }
+
 
