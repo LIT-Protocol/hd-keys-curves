8f62be7884adeebdb895a442c5eab5abe16048fc
Michael Lodder <mike@litprotocol.com>
2024-01-31 12:48:28 -0700

    add curve25519-dalek group

--- src/lib.rs
+++ src/lib.rs
@@ -113,6 +113,36 @@ impl HDDerivable for ed448_goldilocks_plus::EdwardsPoint {
     }
 }
 
+impl HDDeriver for curve25519_dalek_ml::Scalar {
+    fn create(msg: &[u8], dst: &[u8]) -> Self {
+        let msg = [msg];
+        let dst = [dst];
+        let mut expander = ExpandMsgXmd::<sha2::Sha512>::expand_message(&msg, &dst, 64)
+            .expect("expand_message failed");
+        let mut okm = [0u8; 64];
+        expander.fill_bytes(&mut okm);
+        curve25519_dalek_ml::Scalar::from_bytes_mod_order_wide(&okm)
+    }
+}
+
+impl HDDerivable for curve25519_dalek_ml::EdwardsPoint {
+    fn sum_of_products(points: &[Self], scalars: &[Self::Scalar]) -> Self {
+        points.iter().zip(scalars.iter()).fold(
+            curve25519_dalek_ml::EdwardsPoint::default(),
+            |acc, (pt, sc)| acc + *pt * *sc,
+        )
+    }
+}
+
+impl HDDerivable for curve25519_dalek_ml::RistrettoPoint {
+    fn sum_of_products(points: &[Self], scalars: &[Self::Scalar]) -> Self {
+        points.iter().zip(scalars.iter()).fold(
+            curve25519_dalek_ml::RistrettoPoint::default(),
+            |acc, (pt, sc)| acc + *pt * *sc,
+        )
+    }
+}
+
 impl HDDeriver for vsss_rs::curve25519::WrappedScalar {
     fn create(msg: &[u8], dst: &[u8]) -> Self {
         let msg = [msg];
